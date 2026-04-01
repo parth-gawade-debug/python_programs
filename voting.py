c3a83cc11d4368091f6fde35f867c5a3fd7e7979
Parth Gawade <gawadeparth777@gmail.com>
2026-04-01 13:05:40 +0000

    Used simple if else condition for voting allowences

---
+++ voting.py
@@ -0,0 +1,6 @@
+# this program will find if you are eligible for voting.
+age=int (input("enter your age:"))
+if(age>18) :
+    print("congrats you can vote")
+else:
+    print("nope you cant vote ")
