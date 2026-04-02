137709ec42cdf76dc952199c466bae198d159201
Parth Gawade <gawadeparth777@gmail.com>
2026-04-02 05:54:13 +0000

    created a simple even and odd identifier

---
+++ even_odd.py
@@ -0,0 +1,7 @@
+# write a  program which determines if a number is even or odd.
+a=int(input("enter a number of your choice: "))
+if (a%2==0) :
+    print("It is an even number!")
+else :
+    print("it is an odd number!")
+    
