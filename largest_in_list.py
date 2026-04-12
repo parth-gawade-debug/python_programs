9a0492b8f6cf52bbf5559216de2fa67ab7097fa2
Parth Gawade <gawadeparth777@gmail.com>
2026-04-12 04:32:22 +0000

    sorting of list

---
+++ largest_in_list.py
@@ -0,0 +1,4 @@
+# in this program we will sort the elements from the list and pick the largest 
+list1=[12,45,7,23,89,46]
+list1.sort(reverse=True) # this will sort the list in such a manner(descending) the largest comes first.
+print(list1[0])
